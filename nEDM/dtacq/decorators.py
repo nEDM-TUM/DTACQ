7cba50520b01291134afe468970684e8af62cd4a
Michael Marino <mmarino@gmail.com>
2015-09-18 10:52:03 +0200

    Correct decorators

--- nEDM/dtacq/decorators.py
+++ nEDM/dtacq/decorators.py
@@ -3,14 +3,14 @@ from .digitizer_utils import ReadoutException
 def notRunning(func):
     def _wrap(self, *args, **kw):
         if self.isRunning:
-            raise ReadoutException("Cannot call function while readout running")
+            raise ReadoutException("Cannot call function ({}) while readout running".format(func.__name__))
         return func(self, *args, **kw)
     return _wrap
 
 def isRunning(func):
     def _wrap(self, *args, **kw):
         if not self.isRunning:
-            raise ReadoutException("Cannot call function while readout running")
+            raise ReadoutException("Can only call function ({}) during readout".format(func.__name__))
         return func(self, *args, **kw)
     return _wrap
 
