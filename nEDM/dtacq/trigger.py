fb37e92e31d9027ad5215b1ff5c35f6f5e568288
Michael Marino <mmarino@gmail.com>
2015-09-18 10:54:04 +0200

    Fix trigger read in code, now handles import, etc.

--- nEDM/dtacq/trigger.py
+++ nEDM/dtacq/trigger.py
@@ -13,18 +13,25 @@ class Trigger(object):
 def get_trigger(exec_str=None):
    if not exec_str or exec_str=="":
        return Trigger()
-
    import imp, uuid
-   mod = imp.new_module(str(uuid.uuid4()))
+   import sys
+   aname = str(uuid.uuid4())
+   mod = imp.new_module(aname)
    mod.__dict__["Trigger"] = Trigger
    mod.__dict__["EndReadoutNow"] = EndReadoutNow
    exec exec_str in mod.__dict__
    for name, c in mod.__dict__.items():
        try:
-           if issubclass(c, Trigger) and c != Trigger:
-               logging.info("Using trigger class: " + name)
-               return c()
-       except: pass
+           x = issubclass(c, Trigger)
+       except:
+           continue
+       if x and c != Trigger:
+           logging.info("Using trigger class: " + name)
+           o = c()
+           # If trigger was not defined, the following will throw
+           o.trigger
+           sys.modules[aname] = mod
+           return o
    raise ReadoutException("No Trigger class found")
 
 
